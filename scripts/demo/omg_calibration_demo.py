9582e3ba1c90effc3235e7a6a95cfc19fa999ab5
JFC <jean-francois.cabana.cisssca@ssss.gouv.qc.ca>
2023-03-01 10:06:27 -0500

    Demo calibration
    -Mise à jour du script omg_calibration_demo
    -Ajout des fichiers nécessaires à exécuter la démo
    -Ajout d'un plt.wait dans calibration.py lors de la sélection manuelle des ROI, afin d'éviter que le script passe aux étapes suivantes avant d'être prêt

---
+++ scripts/demo/omg_calibration_demo.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Ce script est sert à démontrer l'utilisation du module calibration de omg_dosimetry.
+Vous pouvez en faire une copie et l'adapter selon vos besoins.
+    
+Écrit par Jean-François Cabana
+jean-francois.cabana.cisssca@ssss.gouv.qc.ca
+2023-03-01
+"""
+
+#%% Importer les librairies
+from omg_dosimetry import calibration
+import os
+
+#%% Définir les informations générales
+info = dict(author = 'Demo Physicien',
+            unit = 'Demo Linac',
+            film_lot = 'XD_1',
+            scanner_id = 'Epson 72000XL',
+            date_exposed = '2023-01-24 16h',
+            date_scanned = '2023-01-25 16h',
+            wait_time = '24 heures',
+            notes = 'Scan en transmission @300ppp'
+           )
+
+path_calib = os.path.join(os.path.dirname(__file__), "files", "calibration") # Dossier racine pour la calibration
+path_scan = os.path.join(path_calib, "scan")                                 # Dossier contenant les images numérisées
+outname = 'Demo_calib_24h_trans_300ppp_0-30Gy_LatCorr_BeamCorr'              # Nom du fichier de calibration à produire
+
+#%% Définir les paramètres de calibration
+# Dose
+doses = [0.00, 100.00, 200.00, 400.00, 650.00, 950.00, 1300.00, 1700.00, 2150.00, 2650.00, 3200.00]  # Doses nominales irradiée sur les films
+output = 1.0                                                                                         # Si nécessaire, correction pour l'output quotidien de la machine 
+
+# Correction latérale
+lateral_correction = True                                   # True pour effectuer une calibration avec correction latérale du scanner (nécessite des longues bandes de films), ou False pour calibration sans correction latérale
+beam_profile = os.path.join(path_calib, "BeamProfile.txt")  # None pour ne pas corriger pour la forme du profile de dose, ou chemin d'accès vers un fichier texte contenant la forme de profile (1ère colonne = position [mm], 2e colonne = dose relative [%])
+
+# Détection des films
+film_detect = False      # True pour tenter une détection automatique des films, ou False pour faire une sélection manuelle
+crop_top_bottom = 650   # Si film_detect = True : Nombre de pixel à cropper dans le haut et le bas de l'image. Peut être nécessaire pour détection automatique si la vitre sur le scanner empêche la détection
+roi_size = 'auto'       # Si film_detect = True : 'auto' pour définir la taille des ROI selon les films, ou [largeur, hauteur] (mm) pour définir une taille fixe.
+roi_crop = 3            # Si film_detect = True et roi_size = 'auto' : Taille de la marge [mm] à appliquer de chaque côté des films pour définir les ROI.
+
+# Filtre image
+filt = 3                # Taille du kernel de filtre médian à appliquer sur les images pour la réduction du bruit
+
+#%% Produire la LUT
+LUT = calibration.LUT(path=path_scan, doses=doses, output=output, lateral_correction=lateral_correction, beam_profile=beam_profile,
+                        film_detect=film_detect, roi_size=roi_size, roi_crop=roi_crop, filt=filt, info=info, crop_top_bottom = crop_top_bottom)
+
+#%% Afficher les résultats et sauvegarde de la LUT           
+LUT.plot_roi()
+LUT.plot_fit(i='mean', fit_type='rational', k=3, ext=3, s=0)
+LUT.publish_pdf(filename=os.path.join(path_calib, outname +'_report.pdf'), open_file=True)
+
+filename = os.path.join(path_calib, outname + '.pkl')
+calibration.save_lut(LUT, filename)
---
+++ scripts/demo/omg_tiff2dose_demo.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from omg_dosimetry import tiff2dose
+import os
+
+#%% Set info
+info = dict(author = 'JFC',
+            unit = 'Salle 2',
+            film_lot = 'C14 XD',
+            scanner_id = 'Epson 72000XL',
+            date_exposed = '2023-01-24 16h',
+            date_scanned = '2023-01-25 10h',
+            wait_time = '18 heures',
+            notes = 'Scan en transmission @300ppp')
+# Set paths
+# ** Atention, si un nom de répertoire commence avec un chiffre, mettre un double \ devant **
+path = "P:\\Projets\\CRIC\\Physique_Medicale\\Films\\Calibrations\\2023-01-23 C14 XD\\18h\\Valid"
+path = "P:\\Projets\\CRIC\\Physique_Medicale\\Films\\QA Patients\\0phy_SRS_multi\\Test"
+path_in = path + '\\Scan'
+lut_file = "P:\\Projets\\CRIC\\Physique_Medicale\\Films\\Calibrations\\C14_calib_18h_trans_300ppp_0-9Gy.pkl"
+fit_type = 'rational'
+ext = '_0-9Gy'
+path_out = os.path.join(path, 'Dose' + ext)
+
+# Set options
+clip = 3000
+rot90 = 0
+
+# Perform tiff2dose conversion
+if not os.path.exists(path_out): os.makedirs(path_out)
+    
+files = os.listdir(path_in)
+for file in files:
+    img_file = os.path.join(path_in, file)
+    filebase, fileext = os.path.splitext(file)
+    if img_file[-7:] != '001.tif': continue
+    if os.path.isdir(img_file): continue
+    
+    gaf1 = tiff2dose.Gaf(path=img_file, lut_file=lut_file, fit_type=fit_type, info=info, clip = clip, rot90=rot90)
+    gaf1.dose_opt.save(os.path.join(path_out, filebase[:-4] + ext + '_opt.tif')) 
+    report_doseFilm = os.path.join(path_out, filebase[:-4] + ext + '.pdf')
+    gaf1.publish_pdf(filename=report_doseFilm, open_file=True)
