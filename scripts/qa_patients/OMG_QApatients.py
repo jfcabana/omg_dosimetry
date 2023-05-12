1690f8c1da6fb70162ba914f807dc732d06e4ff6
JFC <jean-francois.cabana.cisssca@ssss.gouv.qc.ca>
2023-05-12 16:45:04 -0400

    Update OMG_QApatients.py

--- scripts/qa_patients/OMG_QApatients.py
+++ scripts/qa_patients/OMG_QApatients.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 import openpyxl
+import time
 
 from omg_dosimetry import tiff2dose, analysis
 path_films = 'P:\\Projets\\CRIC\\Physique_Medicale\\Films\\'
@@ -8,46 +9,46 @@ path_films = 'P:\\Projets\\CRIC\\Physique_Medicale\\Films\\'
 if __name__ == '__main__':
 #%% ################################## Entrer vos informations ci-dessous ##################################
     #### Infos film ####
-    info = dict(author = 'JMo',                 # Initiales du phys qui a fait la mesure
+    info = dict(author = 'DS',                 # Initiales du phys qui a fait la mesure
                 unit = '1',                     # Salle
-                film_lot = 'C11',               # Lot de film utilisé. Important car détermine la calibration à prendre.
+                film_lot = 'C14',               # Lot de film utilisé. Important car détermine la calibration à prendre.
                 scanner_id = 'Epson 12000XL',   # Ne change pas
-                date_exposed = '2022-03-29',    # Date d'exposition des films
-                date_scanned = '2022-03-30',    # Date de numérisation des films
+                date_exposed = '2023-05-02',    # Date d'exposition des films
+                date_scanned = '2023-05-03',    # Date de numérisation des films
                 wait_time = '16h',              # Temps d'attente entre l'exposition et la numérisation
-                notes = 'Scan en transmission à 72ppp') 
+                notes = 'Scan en transmission ') 
 
     #### Infos plan ####
-    ID_patient = 'Goret_VersaHD_FFF'         # ID du patient
-    ID_plan = 'D1A'           # ID du plan
-    prescription = 2100           # Dose prescription (cGy / fraction)
+    ID_patient = 'phy_sbrtoligo_e2e'         # ID du patient
+    ID_plan = 'b1b'           # ID du plan
+    prescription = 700           # Dose prescription (cGy / fraction)
 
     ### Fantôme (Décommenter la ligne qui correspond au fantôme utilisé) ###
     # fantome = 'Goret sag SRS'         # Le Petit Goret avec film sagittal
-    fantome = 'Goret coro SRS'       # Le Petit Goret avec film coronal
+ #   fantome = 'Goret coro SRS'       # Le Petit Goret avec film coronal
 #    fantome = 'Quasar'           # Quasar avec film sagittal
 #    fantome = 'Baby Blue SRS'    # Baby Blue montage sprécial pour SRS multi
 #    fantome = 'eau solide'       # Fantôme QA IMRT Classique
 #    fantome = 'output electrons'       # Fantôme pour output électrons. Utiliser lorsque mesure film électrons préparé avec script QA électrons.
-#    fantome = 'autre'            # Si besoin de définir des paramètres de fantôme particuliers
+    fantome = 'autre'            # Si besoin de définir des paramètres de fantôme particuliers
 
     #### Choisir les étapes à effectuer ####
     rot_scan = 0                  # True (1) pour appliquer une rotation de 90 degrés sur l'image (si l'image de scan est verticale), False (0) sinon
     tiff_2_dose = 0               # True (1) pour convertir les scans en dose, False (0) si cette étape est déjà faite
     tiff_2_dose_show_pdf = 0      # True (1) pour ouvrir le PDF de rapport de conversion en dose, False (0) sinon
-    crop_film = 0                 # True (1) pour cropper l'image du film avant l'analyse, False (0) sinon
+    crop_film = 1                 # True (1) pour cropper l'image du film avant l'analyse, False (0) sinon
     dose_2_analysis = 1           # True (1) pour effectuer l'analyse Gamma, False(0) sinon
     dose_2_analysis_show_pdf = 0  # True (1) pour ouvrir le PDF de rapport d'analyse gamme, False (0) sinon
     save_analysis = 0             # True (1) pour sauvegarder les résultats de l'analyse en format pkl pour pouvoir les recharger plus tard
-    get_profile_offsets = 1       # True (1) pour lancer l'outil d'analyse de décalage des profiles
+    get_profile_offsets = 0       # True (1) pour lancer l'outil d'analyse de décalage des profiles
 
     #### Choisir une méthode de normalisation (Décommenter la ligne selon la normalisation voulue) ###
-    normalisation = 1.0           # Chiffre (float): applique ce facteur de normalisation. Laisser à 1.0 si aucune normalisation
+    #normalisation = 1.0           # Chiffre (float): applique ce facteur de normalisation. Laisser à 1.0 si aucune normalisation
     # normalisation = 'ref_roi'    # 'ref_roi': sélectionne une ROI sur le film et normalisation par rapport à la dose de référence
     # normalisation = 'norm_film'  # 'norm_film': sélectionne le film de normalisation (dans le même scan) pour calculer le facteur par rapport à une dose attendue
     # norm_film_dose = 1500        # Si normalisation = 'norm_film', la dose moyenne [cGy] sur une ROI au centre du film
-    # normalisation = 'isodose'    # 'isodose': Applique un facteur de normalisation pour faire correspondre la dose moyenne pour tout ce qui est suppérieur à norm_isodose
-    # norm_isodose = prescription * 0.8  # Dose [cGy] à utiliser pour la normalisation de type 'isodose'
+    normalisation = 'isodose'    # 'isodose': Applique un facteur de normalisation pour faire correspondre la dose moyenne pour tout ce qui est suppérieur à norm_isodose
+    norm_isodose = prescription * 1.2  # Dose [cGy] à utiliser pour la normalisation de type 'isodose'
 
     #################################### Paramètres automatiques, modifiables au besoin #############################################
     #### Paramètres de conversion en dose ####
@@ -55,8 +56,8 @@ if __name__ == '__main__':
     if info['film_lot'] == 'C10': lut_file = os.path.join(path_calib, 'C10_calib_24h_trans_vitre_0-10Gy.pkl')
     elif info['film_lot'] == 'C11': lut_file = os.path.join(path_calib, 'C11-XD_calib_18h_trans_300ppp_0-30Gy.pkl')
     elif info['film_lot'] == 'C13': lut_file = os.path.join(path_calib, 'C13_calib_18h_trans_300ppp_0-3Gy.pkl')
-    # elif info['film_lot'] == 'C14': lut_file = os.path.join(path_calib, 'C14_calib_18h_trans_300ppp_0-9Gy_LatCorr_BeamCorr.pkl')
-    elif info['film_lot'] == 'C14': lut_file = os.path.join(path_calib, 'C14_calib_18h_trans_300ppp_0-30Gy_LatCorr_BeamCorr.pkl')
+    elif info['film_lot'] == 'C14': lut_file = os.path.join(path_calib, 'C14_calib_18h_trans_300ppp_0-9Gy_LatCorr_BeamCorr.pkl')
+#    elif info['film_lot'] == 'C14': lut_file = os.path.join(path_calib, 'C14_calib_18h_trans_300ppp_0-30Gy_LatCorr_BeamCorr.pkl')
 #    elif info['film_lot'] == 'C14': lut_file = os.path.join(path_calib, 'C14_calib_18h_trans_300ppp_0-9Gy.pkl')
 
     clip = prescription * 1.5  # Dose maximum où couper la dose [cGy]. Si 'None', n'applique pas de threshold.
@@ -66,7 +67,7 @@ if __name__ == '__main__':
     shift_x = 0             # Si décalage connu en X [mm]
     shift_y = 0             # Si décalage connu en Y [mm]
     rot = 0                 # Si rotation connue [degrés] (+: rotation anti-horaire)
-    
+
     # flipLR: Symétrie miroir horizontale du film; flipUD: Symétrie vertical; rot90: # de rotations de 90 degrés
     # markers_center: corrdonnées du centre des marques sur le CT de référence [RL, IS, PA] en mm
     if fantome == 'Goret coro SRS':
@@ -95,18 +96,26 @@ if __name__ == '__main__':
     
     if fantome == 'autre':
         flipLR, flipUD, rot90 = True, False, 1
-        markers_center = [0.0, 0.0, 0.0]          # À définir selon besoins particuliers
+        #markers_center = [0.0, 0.0, 0.0]          # À définir selon besoins particuliers
+        #markers_center = [0.5, -345.0, 254.6]     #sbrtoligo a1a 
+        #markers_center = [2, -344, 255.5]     #sbrtoligo a1a 
+        markers_center = [4.4, -269.0, 259.5]     #sbrtoligo b1a 
     
     #### Paramètres analyse Gamma ####
     doseTA_1, distTA_1 = 3, 3    # 1ère analyse: Dose et distance to agreement
     doseTA_2, distTA_2 = 2, 2    # 2e analyse: Dose et distance to agreement
     threshold = 0.20             # Seuil de basses doses (0.2 = ne considère pas les doses < 20% du max de la dose ref)
     norm_val = prescription      # 'max' pour normaliser par rapport à la dose maximum, ou entre une dose absolue en cGy pour normaliser sur une autre valeur  
-
+    max_gamma = 1.1             # Valeur supérieure de gamma à partir de laquelle le calcul est interrompu. None pour pas de limite. Pas d'impact sur le taux de passage, mais sur gamma moyen oui.
+    random_subset = 0.5         # 'None' pour calcul gamma sur toute la grille, ou float 0-1 pour échantillonner aléatoirement la grille sur une fraction donnée
+    x,  y = 'max', 'max'        # Position du profil par défaut. 'None' pour centré sur les marques. 'max' pour centré sur dose max.
+    
+    
     # Pour les cas SRS, les critères Gamma sont différents
     if 'SRS' in fantome:
         doseTA_1, distTA_1 = 5, 1
         doseTA_2, distTA_2 = 5, 0.5 
+        
 #%% ################################## NE RIEN CHANGER PLUS BAS ########################################
 
     #%% ################################### Set paths and file names ########################################
@@ -151,30 +160,34 @@ if __name__ == '__main__':
         print("      Écart médian: {:.2f}% (seuil = {:0.1f} * {} cGy = {} cGy)".format(medianDiff, thresh, prescription, seuil))
         print("===============================================================\n")
 
+        xoff=800   # a2000   b800
+        yoff=800   # a1000   b800
+
         #%% Perform gamma analysis
         filename= '{}_Facteur{:.2f}_Filtre{}_Gamma{}%-{}mm_report.pdf'.format(filebase, film.film_dose_factor,film_filt, doseTA_1, distTA_1)
         fileout=os.path.join(path_analyse, filename)
 
-        film.gamma_analysis(doseTA=doseTA_1, distTA=distTA_1, threshold=threshold, norm_val=norm_val, film_filt=film_filt)
+        film.gamma_analysis(doseTA=doseTA_1, distTA=distTA_1, threshold=threshold, norm_val=norm_val, film_filt=film_filt, max_gamma=max_gamma, random_subset=random_subset)
         print("\n===================== Analyse Gamma ========================")
         print("      Gammma {}% {}mm: Taux de passage={:.2f}%; Moyenne={:.2f}".format(doseTA_1, distTA_1, film.GammaMap.passRate, film.GammaMap.mean))
         print("==============================================================\n")
-#        film.show_results()
-        film.publish_pdf(fileout, open_file=dose_2_analysis_show_pdf, show_hist=True, show_pass_hist=True, show_varDistTA=False, show_var_DoseTA=False, x=None, y=None)
+        film.show_results(x=x, y=y)
+        film.publish_pdf(fileout, open_file=dose_2_analysis_show_pdf, show_hist=True, show_pass_hist=True, show_varDistTA=False, show_var_DoseTA=False, x=xoff, y=yoff)
 
         gamma1_pass = film.GammaMap.passRate
         gamma1_mean = film.GammaMap.mean
+
 
         #%% Perform gamma analysis
         filename= '{}_Facteur{:.2f}_Filtre{}_Gamma{}%-{}mm_report.pdf'.format(filebase, film.film_dose_factor,film_filt, doseTA_2, distTA_2)
         fileout=os.path.join(path_analyse, filename)
 
-        film.gamma_analysis(doseTA=doseTA_2, distTA=distTA_2, threshold=threshold, norm_val=norm_val, film_filt=film_filt)
+        film.gamma_analysis(doseTA=doseTA_2, distTA=distTA_2, threshold=threshold, norm_val=norm_val, film_filt=film_filt, max_gamma=max_gamma, random_subset=random_subset)
         print("\n===================== Analyse Gamma ========================")
         print("      Gammma {}% {}mm: Taux de passage={:.2f}%; Moyenne={:.2f}".format(doseTA_2, distTA_2, film.GammaMap.passRate, film.GammaMap.mean))
         print("==============================================================\n")
-        film.show_results()
-        film.publish_pdf(fileout, open_file=dose_2_analysis_show_pdf, show_hist=True, show_pass_hist=True, show_varDistTA=False, show_var_DoseTA=False, x=None, y=None)
+        film.show_results(x=x, y=y)
+        film.publish_pdf(fileout, open_file=dose_2_analysis_show_pdf, show_hist=True, show_pass_hist=True, show_varDistTA=False, show_var_DoseTA=False, x=xoff, y=yoff)
 
         gamma2_pass = film.GammaMap.passRate
         gamma2_mean = film.GammaMap.mean
